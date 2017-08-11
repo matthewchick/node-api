ca8b849ddd3849ce6b8d97342d220e7efb7c393f
Matthew Chick <chikmatthew@gmail.com>
2017-08-11 13:49:18 +1000

    add post /todos route and refactor mongoose

--- server/server.js
+++ server/server.js
@@ -1,12 +1,40 @@
 
 /*
-create api for model, mongoose connection
-
+1. create api for model, mongoose connection
+2. sudo npm i express@latest body-parser@latest --save
+3. httpstatuses.com
 */
 // deconstructing at ES6
+var express = require('express');
+// Parse incoming request bodies in a middleware before your handlers,
+// available under the req.body property.
+var bodyParser = require('body-parser');
+
 var {mongoose} = require('./db/mongoose.js');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+  var todo = new Todo({
+    text: req.body.text
+  })
+  console.log(req.body.text);
+  todo.save().then((doc) => {
+    res.send(doc);
+    console.log(JSON.stringify(doc, undefined, 2));
+  }, (e) => {
+    console.log('Unable to save todo', e);
+    res.status(400).send(e);
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Started on port 3000');
+});
 
 /* move to db/mongoose.js
 var mongoose = require('mongoose');
@@ -53,6 +81,7 @@ var User = mongoose.model('User', {
   }
 });
 */
+/*
 var otherTodo = new Todo({
   text: 'This is a test',
   completed: true,
@@ -74,3 +103,4 @@ user.save().then((doc) => {
 }, (e) => {
   console.log('Unable to save user', e);
 });
+*/
