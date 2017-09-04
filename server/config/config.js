925e27d7df0d5bdf8261f10b8a2da00d3f9dad4d
Matthew Chick <chikmatthew@gmail.com>
2017-09-04 12:46:36 +1000

    Fix Unhandled promise rejection (rejection id: 2): undefined

--- server/config/config.js
+++ server/config/config.js
@@ -1,13 +1,16 @@
-
 // test, production and development
-var env = process.env.NODE_ENV ||'development';
+// mongodb://<dbuser>:<dbpassword>@ds119044.mlab.com:19044/todoapp => to mLab.com
+// username: chikmatthew
+// mongo ds119044.mlab.com:19044/todoapp -u <dbuser> -p <dbpassword>
+
+var env = process.env.NODE_ENV || 'development';
 
 console.log('env ****', env);
 
 if (env === 'development') {
   process.env.PORT = 3000;
-  process.env.MongoDB_URI ='mongodb://localhost:27017/TodoApp';
+  process.env.MongoDB_URI = 'mongodb://localhost:27017/TodoApp';
 } else if (env === 'test') {
   process.env.PORT = 3000;
-  process.env.MongoDB_URI ='mongodb://localhost:27017/TodoAppTest';
+  process.env.MongoDB_URI = 'mongodb://localhost:27017/TodoAppTest';
 }
--- server/server.js
+++ server/server.js
@@ -1,4 +1,3 @@
-
 /*
  1. create api for model, mongoose connection
  2. sudo npm i express@latest body-parser@latest --save
@@ -24,14 +23,15 @@ const _ = require('lodash');
 // Parse incoming request bodies in a middleware before your handlers,
 // available under the req.body property.
 var bodyParser = require('body-parser');
-var {ObjectID} = require('mongodb');
+var { ObjectID } = require('mongodb');
 
 // const is better than var
-const {mongoose} = require('./db/mongoose.js');
-var {Todo} = require('./models/todo');
-var {User} = require('./models/user');
+const { mongoose } = require('./db/mongoose.js');
+var { Todo } = require('./models/todo');
+var { User } = require('./models/user');
 
 var app = express();
+// deploy to Heroku
 const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
@@ -40,31 +40,37 @@ app.post('/todos', (req, res) => {
   var todo = new Todo({
     text: req.body.text,
     completedAt: req.body.completedAt
-  })
+  });
   //console.log(req.body.text);
   //console.log(req.body.completedAt);
-  todo.save().then((doc) => {
-    res.send(doc);
-    //console.log(doc);
-    //console.log(JSON.stringify(doc, undefined, 2));
-  }, (e) => {
-    //console.log('Unable to save todo', e);
-    res.status(400).send(e);
-  });
+  todo.save().then(
+    doc => {
+      res.send(doc);
+      //console.log(doc);
+      //console.log(JSON.stringify(doc, undefined, 2));
+    },
+    e => {
+      //console.log('Unable to save todo', e);
+      res.status(400).send(e);
+    }
+  );
 });
 
 app.get('/todos', (req, res) => {
-  Todo.find().then((todos) => {
-    res.send({todos});
-  }, (e) => {
-    res.status(400).send(e);
-  });
+  Todo.find().then(
+    todos => {
+      res.send({ todos });
+    },
+    e => {
+      res.status(400).send(e);
+    }
+  );
 });
 
 app.get('/todos/:id', (req, res) => {
   var id = req.params.id;
 
-  if (!ObjectID.isValid(id)){
+  if (!ObjectID.isValid(id)) {
     return res.status(404).send();
   }
   // Valid id using isValid
@@ -72,39 +78,44 @@ app.get('/todos/:id', (req, res) => {
 
   // findById
 
-  Todo.findById(id).then((todo) => {
-    if (!todo){
-      return res.status(404).send();
+  Todo.findById(id).then(
+    todo => {
+      if (!todo) {
+        return res.status(404).send();
+      }
+      res.send({ todo });
+    },
+    e => {
+      res.status(400).send(e);
     }
-    res.send({todo});
-  }, (e) => {
-    res.status(400).send(e);
-  });
+  );
 });
 
 app.delete('/todos/:id', (req, res) => {
   //get the id
   var id = req.params.id;
   //validate the id -> not valid? return 404
-  if (!ObjectID.isValid(id)){
+  if (!ObjectID.isValid(id)) {
     return res.status(404).send();
   }
   //remove todo by id -> success -> error
-  Todo.findByIdAndRemove(id).then((todo) => {
-    if (!todo){
-      return res.status(404).send();
-    }
-    res.send({todo});
-  }).catch((e)=> {
-    res.status(400).send();
-  });
+  Todo.findByIdAndRemove(id)
+    .then(todo => {
+      if (!todo) {
+        return res.status(404).send();
+      }
+      res.send({ todo });
+    })
+    .catch(e => {
+      res.status(400).send();
+    });
 });
 //update
 app.patch('/todos/:id', (req, res) => {
   var id = req.params.id;
   var body = _.pick(req.body, ['text', 'completed']);
 
-  if (!ObjectID.isValid(id)){
+  if (!ObjectID.isValid(id)) {
     return res.status(404).send();
   }
 
@@ -115,48 +126,57 @@ app.patch('/todos/:id', (req, res) => {
     body.completedAt = null;
   }
 
-  Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
-    if (!todo){
-      return res.status(404).send();
-    }
-    res.send({todo});   //res.send({todo: todo})
-  }).catch((e)=> {
-    res.status(400).send();
-  });
-})
+  Todo.findByIdAndUpdate(id, { $set: body }, { new: true })
+    .then(todo => {
+      if (!todo) {
+        return res.status(404).send();
+      }
+      res.send({ todo }); //res.send({todo: todo})
+    })
+    .catch(e => {
+      res.status(400).send();
+    });
+});
 // POST /users
 app.post('/users', (req, res) => {
   var body = _.pick(req.body, ['email', 'password']);
-  var user = new User(body)
+  var user = new User(body);
 
   //User.findByToken
   //user.generateAuthToken
-  user.save().then(() => {
-    return user.generateAuthToken();
-    //res.send(user);
-  }).then((token) => {
-    res.header('x-auth', token).send(user);
-  }).catch((e) => {
-    res.status(400).send(e);
-  });
+  user
+    .save()
+    .then(() => {
+      return user.generateAuthToken();
+      //res.send(user);
+    })
+    .then(token => {
+      res.header('x-auth', token).send(user);
+    })
+    .catch(e => {
+      res.status(400).send(e);
+    });
 });
 // Set the private route
 app.get('/users/me', (req, res) => {
   var token = req.header('x-auth');
 
-  User.findByToken(token).then((user) => {
+  User.findByToken(token).then(user => {
     if (!user) {
-
     }
     res.send(user);
+  })
+  .catch(e => {
+    res.status(400).send(e);
   });
 });
 
+// deploy to Heroku
 app.listen(PORT, () => {
   console.log(`Started on port ${PORT}`);
 });
 
-module.exports = {app};
+module.exports = { app };
 
 /* move to db/mongoose.js
 var mongoose = require('mongoose');
